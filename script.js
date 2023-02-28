1fa9ce34e005351cd32a496a522482e9d7e0c25c
Kyle Moreen <Kyle@Kyles-MacBook-Pro-5.local>
2023-02-28 13:11:44 -0500

    added popup confirming form submission

--- script.js
+++ script.js
@@ -46,3 +46,44 @@ function parseTime(delta) {
     const seconds = Math.floor(remaining / 1000);
     displaySecond.innerText = `${seconds} \n Seconds`;
 }
+
+function submitPetition() {
+    disableSubmit();
+    disableForm();
+    createAlert();
+}
+
+function disableSubmit() {
+    const submitButton = document.getElementById('submit-button');
+    submitButton.setAttribute('disabled', 'disabled');
+}
+
+function disableForm() {
+    const form = document.getElementById('form');
+    form.setAttribute('disabled', 'disabled');
+}
+
+function createAlert() {
+    const overlay = document.createElement('div');
+    overlay.setAttribute('class', 'overlay');
+    overlay.setAttribute('id', 'overlay');
+    document.body.appendChild(overlay);
+
+    const alertContainer = document.createElement('div');
+    alertContainer.setAttribute('class', 'alert-container');
+    alertContainer.setAttribute('id', 'alert-container');
+    document.body.appendChild(alertContainer);
+
+    const alert = document.createElement('div');
+    alert.setAttribute('class', 'alert');
+    alert.setAttribute('id', 'alert');
+    alert.innerHTML = 'Your response has been recorded. Definitely. For sure. No response has ever been more recorded than yours was just now.'
+    alertContainer.appendChild(alert);
+
+    const okButton = document.createElement('button');
+    okButton.setAttribute('class', 'ok-button');
+    okButton.setAttribute('id', 'ok-button');
+    okButton.setAttribute('onClick', 'location.reload()');
+    okButton.innerText = 'OK';
+    alertContainer.appendChild(okButton);
+}
